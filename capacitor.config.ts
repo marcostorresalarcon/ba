ef3d6f111c17ffb9f773bd35f113b5d9f051c32b
Marcos Torres <marcostor13@gmail.com>
2025-11-24 19:51:29 -0500

    fix: update appId and PRODUCT_BUNDLE_IDENTIFIER to match new naming convention

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.bakitchebathdesigns.appprod',
+  appId: 'com.bakitchenandbathdesigns.app',
   appName: 'App',
   webDir: 'dist/ba/browser'
 };
