5d174302a691408d3654db7134654378ec8ced77
Marcos Torres <marcostor13@gmail.com>
2026-01-05 17:27:38 -0500

    refactor: update NativeVideoPicker plugin method declaration to remove promise return type for pickVideo; streamline method signature for improved clarity

--- ios/App/App/NativeVideoPicker.m
+++ ios/App/App/NativeVideoPicker.m
@@ -3,6 +3,6 @@
 
 // Registrar el plugin NativeVideoPicker para que Capacitor lo reconozca
 CAP_PLUGIN(NativeVideoPicker, "NativeVideoPicker",
-           CAP_PLUGIN_METHOD(pickVideo, CAPMethodReturnPromise);
+           CAP_PLUGIN_METHOD(pickVideo);
 )
 
