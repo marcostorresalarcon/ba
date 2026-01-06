8a2ef5bb24da7a5c9dcae1e2d45d97495292efe4
Marcos Torres <marcostor13@gmail.com>
2026-01-06 10:22:40 -0500

    chore: remove NativeVideoPicker plugin files from the iOS project as functionality is replaced by Camera.pickImages; update Xcode project settings accordingly

--- ios/App/App/NativeVideoPicker.m
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <Capacitor/Capacitor.h>
-
-// Registrar el plugin NativeVideoPicker para que Capacitor lo reconozca
-CAP_PLUGIN(NativeVideoPicker, "NativeVideoPicker",
-           CAP_PLUGIN_METHOD(pickVideo, CAPPluginReturnPromise);
-)
-
